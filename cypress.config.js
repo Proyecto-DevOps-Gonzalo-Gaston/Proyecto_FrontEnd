508d1382307c4ffddbff6a54923ee80807180bc9
root <root@localhost.localdomain>
2024-07-16 16:40:58 -0300

    intentando solucionar error en Run cypress tests v2

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  baseUrl: "http://stg-fe-gg.s3-website-us-east-1.amazonaws.com",
+  video: false,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+  integrationFolder: "cypress/integration"
+  // Agrega aquí cualquier otra configuración que necesites migrar
+};
